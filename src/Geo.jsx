235ecc5713a2b0855a54450446b47cf4112bbf48
JodieAddis <jodie.addis.92@gmail.com>
2023-08-28 16:58:44 +0200

    Fin du menu déroulant. Positionnement correcte des éléments. Affichage des données correct après input.

--- src/Geo.jsx
+++ src/Geo.jsx
@@ -3,170 +3,116 @@ import config from "./config";
 import { Weather } from "./Weather_2";
 import list from "./assets/image/list.svg";
 import close from "./assets/image/close.svg";
-
-// export const GeoLocation = () => {
-//     const [location, setLocation] = useState("");
-
-//     const [cityName, setCityName] = useState(""); //State pour le nom
-//     const [lat, setLat] = useState(null); //State pour la latitude
-//     const [lon, setLon] = useState(null); //State pour la longitude
-
-//     const disableSearch = location.trim() === ""; //locationSearch.trim() enlève les espaces vides au début et à la fin de la chaîne (nettoyage de la saisie)
-
-//     const API_KEY = config.apiKey;
-
-//     const fetchData = async () => {
-//         try {
-//             const locationApi = await fetch(
-//                 `http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=1&appid=${API_KEY}`
-//             );
-//             const locationData = await locationApi.json();
-//             const locationCity = locationData[0];
-//             console.log(locationCity);
-
-//             setCityName(locationCity.name);
-//             setLat(locationCity.lat);
-//             setLon(locationCity.lon);
-
-//             console.log(cityName, lat, lon);
-
-//             setData(locationCity);
-//         } catch {
-//             // console.log("error");
-//         }
-//     };
-
-//     useEffect(() => {
-//         fetchData();
-//     }, [location]);
-
-//     const handleSubmit = (e) => {
-//         e.preventDefault();
-//         fetchData();
-//     };
-
-//     return (
-//         <>
-//             <Weather lat={lat} lon={lon} />
-//             <form
-//                 method="get"
-//                 // onSubmit={(event) => event.preventDefault()}
-//                 onSubmit={handleSubmit}
-//                 className="flex-col justify-center"
-//             >
-//                 <input
-//                     type="text"
-//                     name=""
-//                     id=""
-//                     placeholder="Enter city "
-//                     value={location}
-//                     onChange={(e) => setLocation(e.target.value)}
-//                 />
-//                 <div>
-//                     <button
-//                         onClick={handleSubmit}
-//                         disabled={disableSearch}
-//                         className="text-center bg-white rounded py-2 px-3 m-5"
-//                     >
-//                         Search
-//                     </button>
-//                 </div>
-//             </form>
-//         </>
-//     );
-// };
+import search from "./assets/image/search.svg";
+import sunshine from "./assets/image/sunshine.jpg";
 
 export const GeoLocation = () => {
-    const [location, setLocation] = useState(""); //Nom de la ville rentrée par l'utilisateur
-    const [inputLocation, setInputLocation] = useState("");
-    const [dataAPI, setDataAPI] = useState();
+    const [location, setLocation] = useState("");
+    const [inputLocation, setInputLocation] = useState(""); //Récupération de l'input
+
+    const [openNav, setOpenNav] = useState(false); //Etat ouverture de la navBar
+
+    const [lat, setLat] = useState(null);
+    const [lon, setLon] = useState(null);
+
     const API_KEY = config.apiKey;
 
-    const [hamburger, setHamburger] = useState(false);
-
-    // useEffect(() => {
-    //     fetch(
-    //         `http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=1&appid=${API_KEY}`
-    //     )
-    //         .then((response) => response.json()) //Stockage dans le json
-    //         .then((result) => {
-    //             setDataAPI(result);
-    //         })
-    //         .catch((error) => {
-    //             console.log(error);
-    //         });
-    // }, [location]);
-
-    //Fonction permettant de récupérer la valeur de l'input + mise à jour du state correspondant
     const handleInputLocation = (e) => {
         setInputLocation(e.target.value);
     };
 
-    // Fonction qui met à jour le state de Location lorsque le formulaire est soumis
     const handleSubmit = (e) => {
         e.preventDefault();
         setLocation(inputLocation);
-        setInputLocation(""); //Ré-initialise la valeur
+        closeNavBar(); //Ferme la navbar après le submit
     };
 
-    //Fonction pour affichage de la navBar au click de l'icone hamburger
-    const openNav = () => {
-        setHamburger(true);
+    //Fonction qui controle l'ouverture et la fermeture de la navbar
+    const toggleNavBar = () => {
+        setOpenNav(!openNav);
     };
+
+    const closeNavBar = () => {
+        setOpenNav(false); //On remet sur false pour fermer la navbar
+    };
+
+    useEffect(() => {
+        if (location) {
+            // Ajout de la condition pour éviter les requêtes vides
+            fetch(
+                `http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=1&appid=${API_KEY}`
+            )
+                .then((response) => response.json())
+                .then((result) => {
+                    const resultAPI = result[0];
+                    const latitude = resultAPI.lat;
+                    const longitude = resultAPI.lon;
+
+                    // console.log(result[0]);
+                    // console.log(latitude);
+                    // console.log(longitude);
+
+                    setLat(latitude);
+                    setLon(longitude);
+                    setInputLocation("");
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+        }
+    }, [location, API_KEY]);
 
     return (
         <>
-            <header className="flex justify-between bg-indigo-500 w-screen px-3.5 py-3.5 ">
+            <header className=" bg-indigo-500 w-screen px-3.5 py-3.5 rounded-b-xl absolute z-10">
                 <div>
-                    <button onClick={openNav}>
-                        <img
-                            src={list}
-                            alt="hamburger icon"
-                            className="invert w-8"
-                        />
-                    </button>
-                    {hamburger && (
-                        <nav className="navigation">
-                            <div>
-                                <button>
-                                    <img src={close} alt="close icon" />
-                                </button>
-                            </div>
-                            <div>
+                    <div className="flex justify-between">
+                        <button onClick={toggleNavBar}>
+                            <img
+                                src={openNav ? close : list}
+                                alt="menu icon"
+                                className="invert w-4"
+                            />
+                        </button>
+                        <h1 className="text-indigo-50 text-2xl font-semibold">
+                            Logo
+                        </h1>
+                    </div>
+                    {openNav && (
+                        <nav className="navigation flex justify-center max-w-screen-xl my-10">
+                            <div className="text-center">
                                 <form
                                     method="get"
                                     onSubmit={handleSubmit}
-                                    className="flex flex-col"
+                                    className="flex justify-center"
                                 >
                                     <input
                                         type="text"
                                         name="location"
                                         id="input_location"
+                                        placeholder="Choose a city"
+                                        className="text-md"
                                         value={inputLocation}
                                         onChange={handleInputLocation}
                                     />
                                     <button
                                         type="submit"
-                                        className="text-center text-white"
+                                        className=" flex self-center w-12 invert ml-3"
                                     >
-                                        Search
+                                        <img src={search} alt="search icon" />
                                     </button>
                                 </form>
                             </div>
                         </nav>
                     )}
                 </div>
-                <div>
-                    <h1 className="text-indigo-50 text-2xl font-semibold">
-                        Logo
-                    </h1>
-                </div>
             </header>
-            <main className="bg-gray-800">
+
+            <main className="bg-gray-800 py-20">
                 {location && (
-                    <h1 className="text-white text-3xl text-center">
-                        {location}
-                    </h1>
+                    <>
+                        <Weather lat={lat} lon={lon} />
+                    </>
                 )}
             </main>
         </>
@@ -174,7 +120,7 @@ export const GeoLocation = () => {
 };
 
 /*
-- Mise de l'input dans une NavBar : 
+- Mise de l'input dans une NavBar : Ok
 - Stockage de la dernière ville dans le localStorage : 
 
 */
--- src/Weather_2.jsx
+++ src/Weather_2.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-// import { GeoLocation } from "./Geo";
+import { GeoLocation } from "./Geo";
 import config from "./config";
 
 const API_KEY = config.apiKey;
@@ -12,15 +12,18 @@ export const Weather = ({ lat, lon }) => {
     const [data, setData] = useState();
 
     useEffect(() => {
-        setWeather({ ...weather, isLoading: true });
+        setWeather((prevWeather) => ({ ...prevWeather, isLoading: true }));
         fetch(
             `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
         )
             .then((response) => response.json())
             .then((result) => {
                 setData(result);
-                console.log(result);
-                setWeather({ ...weather, isLoading: false });
+                // console.log(result);
+                setWeather((prevWeather) => ({
+                    ...prevWeather,
+                    isLoading: false,
+                }));
             })
             .catch((error) => {
                 console.error(error);
@@ -33,30 +36,39 @@ export const Weather = ({ lat, lon }) => {
 
     return (
         <>
-            {weather.isLoading && <p>Chargement en cours</p>}
+            {weather.isLoading && (
+                <div className="flex justify-center">
+                    <p className="text-center text-white">
+                        Chargement en cours
+                    </p>
+                    <img src="" alt="" />
+                </div>
+            )}
             {weather.hasError && <p>Une erreur s'est produite</p>}
             {data && data.weather && data.weather.length > 0 && (
                 <>
                     <section className=" flex flex-col text-white">
-                        <h2 className="text-center text-4xl font-bold m-8">
+                        <h2 className="text-center text-3xl font-bold m-8 font-phudu">
                             {data.name}
                         </h2>
-                        <div className=" self-center bg-blue-500 w-56 h-56 rounded-t-full rounded-b-full">
-                            <div className="flex justify-center flex-col text-center my-auto">
-                                <p className="">{data.weather[0].main}</p>
-                                <p className="flex self-center text-6xl font-bold">
+                        <div className=" self-center bg-blue-500 w-56 h-56 rounded-t-full rounded-b-full backdrop-blur-xl">
+                            <div className="flex justify-center flex-col content-around text-center my-auto">
+                                <ul>
+                                    <li className="my-4">
+                                        {data.weather[0].main}
+                                    </li>
+                                    <li className="self-center text-5xl font-bold my-10">
+                                        {data.main.temp}°
+                                    </li>
+                                    <li className="my-4">
+                                        Feels like {data.main.feels_like}°
+                                    </li>
+                                </ul>
+                                {/* <p>{data.weather[0].main}</p>
+                                <p className="self-center text-5xl font-bold">
                                     {data.main.temp}°
                                 </p>
-                                <div>
-                                    {/* <img
-                                        className="min-w-min self-center"
-                                        src={`http://openweathermap.org/img/w/${data.weather[0].icon}.png`}
-                                        alt="weather icon"
-                                    /> */}
-                                </div>
-                                <p className="">
-                                    Feels like {data.main.feels_like}°
-                                </p>
+                                <p>Feels like {data.main.feels_like}°</p> */}
                             </div>
                         </div>
                     </section>
